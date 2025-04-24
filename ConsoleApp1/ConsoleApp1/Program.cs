c9bcb063bb00bbd88274a749c25122c27fac26ec
qkrwjdtlr <132883478+qkrwjdtlr@users.noreply.github.com>
2025-04-24 15:15:43 +0900

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ok!asdasdasddsadsaads!!!!!!!!!!!");
+            Console.WriteLine("ok!aㅇㄴㅁ!!!!!!!!!!");
         }
     }
 }
